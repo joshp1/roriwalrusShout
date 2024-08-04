73212cdcbce0ca722a153b07b09ff625798a2ba2
Josh <joshp1@joshlucy.info>
2024-08-04 03:18:55 -0400

    Update cool.shout.js
    newest update mostly changed version add added a button.

--- cool.shout.js
+++ cool.shout.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Roriwalrus shout
 // @namespace 	roriwalrus.com
-// @version     .063
+// @version     .07
 // @match		 https://www.roriwalrus.com/*
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @description	Adds fun stuff to roriwalrus's shout.
@@ -30,13 +30,31 @@ function bodl() {
   shout.value+="[b]"+"  [/b] ";
 }
 
+// this is for the menu caled memenu
+function memenue () {
+  var visa = document.getElementById ('memene').style.visibility;
+  
+	document.getElementById ('memene').classList.add ('is-active', 'is-complete');
+  document.getElementById ('js-XFUniqueId173').classList.add ('is-active');
+  shout.value+="clickeed " +visa;
+}
 
 // attempt to add button
-var zn = document.getElementsByClassName ('block-header')[4];
+// var zn = document.getElementsByClassName ('block-header')[4];
+
+// new attempt
+var zn = document.getElementsByClassName ('block siropuShoutbox')[0].getElementsByClassName ('block-container')[0].getElementsByClassName ('block-header')[0];
 zn.innerHTML+="<button id='newButon' class='button--iconOnly button--link button button--icon' type='button'>clickme</button>";
+zn.innerHTML+="<button id='meMenu' type='button' class='button--iconOnly button--link button button--icon' aria-haspopup='true'>something</button";
 zn.innerHTML+="<button id='italics' class='button--iconOnly button--link button button--icon'>italics</button>";
 zn.innerHTML+="<button id='bold' class='button--iconOnly button--link button button--icon'>bold</button>";
 
-document.getElementById ('newButon').onclick=function () {aa();};
+var za ='div  class="menu menu--emoji menu--right is-active is-complete menu--up" data-menu="menu" aria-hidden="false" data-xf-init="siropu-shoutbox-smilies-emoji" data-href="/index.php?editor/smilies-emoji" data-load-target=".js-xfSmilieMenuBody" id="js-XFUniqueId174" style="z-index: 206; left: 867.7px; bottom: -1177.4px;">';
+var zb = "<div id= 'memene' class = 'menu menu--up menu--emoji menu--right' aria-hidden = 'false' data-menu = 'menu' style='z-index: 206;'>";
+zn.innerHTML += zb +"<div class = 'menu-row'>someding</div><div class = 'menu-row'>stuff"+"</div></div></div>";
+
+// button bar
+document.getElementById ('newButon').onclick=function () {aa ();};
 document.getElementById ('italics').onclick=function () {itla();};
 document.getElementById ('bold').onclick=function () {bodl();};
+document.getElementById ('meMenu').onclick=function () {memenue ();};
